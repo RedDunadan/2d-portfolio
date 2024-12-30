c1a09d65e17d2b243052189a6048c4a9e815d57c
RedDunadan <105070027+RedDunadan@users.noreply.github.com>
2024-12-30 20:52:57 +0000

    changing player

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
-    "idle-down": 936,
-    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-    "idle-side": 975,
-    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-    "idle-up": 1014,
-    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    "idle-down": 964,
+    "walk-down": { from: 965, to: 967, loop: true, speed: 8 },
+    "idle-side": 1003,
+    "walk-side": { from: 1004, to: 1006, loop: true, speed: 8 },
+    "idle-up": 1042,
+    "walk-up": { from: 1043, to: 1045, loop: true, speed: 8 },
   },
 });
 
